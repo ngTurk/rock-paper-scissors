e832bf182cdbbd5529327a860a7e4a46f863be4b
ngTurk <malik_alturk@hotmail.com>
2020-12-12 13:28:32 +0200

    not needed

--- src/App.js
+++ src/App.js
@@ -22,10 +22,6 @@ class App extends React.Component {
 		return(
 			<div className="main">
 				<div className="rps-container">
-					<form>
-						<input src="" name="name" required />
-						<button type="submit">submit</button>
-					</form>
 					<Header score={this.state.score} />
 					<Game handleScore={this.handleScore} 	/>
 					<small className="made-by"><img src={Bored} alt="bored" /> By Malik Turk</small>
